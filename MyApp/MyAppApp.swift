857a47b40da2d70467f3f6f0db613672596b4653
twixour <twixour@gmail.com>
2023-07-05 13:21:36 +0530

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Raushan Kashyap on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
